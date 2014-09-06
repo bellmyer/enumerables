f664e7dc460fa97f4f919a361b8a2a237be22557
Jaime Bellmyer <online@bellmyer.com>
2014-09-06 09:18:47 -0500

    update chunk example with number list

--- chunk.rb
+++ chunk.rb
@@ -1,14 +1,21 @@
 #!/usr/bin/env ruby
 
-require './lib/pet_inventory'
-require './lib/log_data'
+require './lib/initialize'
 
-inventory = PetInventory.new
-inventory.display
+puts "chunk by even numbers: "
+@numbers.chunk(&:even?).each do |even_status, list|
+  puts "#{even_status}: #{list.join('')}"
+end
 
+puts "chunk more readably by even/odd status: "
+@numbers.chunk{|number| number.even? ? 'even' : 'odd'}.each do |eo_status, list|
+  puts "#{eo_status}: #{list.join('')}"
+end
+
+divider
 
 puts "chunking pets by leg count: "
-pets_by_leg_count = inventory.chunk(&:legs)
+pets_by_leg_count = @inventory.chunk(&:legs)
 
 pets_by_leg_count.each do |leg_count, pet_list|
   puts "  #{leg_count}: #{pet_list.map(&:name).join(',')}"
@@ -16,27 +23,23 @@ end
 
 puts "\n--------\n\n"
 
-File.open('./data/pokey_things.txt') do |f|
-  puts "pokey things: "
-  puts f.read
-  puts
-  
-  puts "chunking pokey things by first letter: "
-  f.seek(0)
+puts "pokey things: "
+puts @pokey_things.read
+puts
 
-  pokey_things_by_first_letter = f.chunk{|line| line[0]}
-  
-  pokey_things_by_first_letter.each do |letter, list|
-    puts "  #{letter}: #{list.map(&:chomp).join(',')}"
-  end
+puts "chunking pokey things by first letter: "
+@pokey_things.seek(0)
+
+pokey_things_by_first_letter = @pokey_things.chunk{|line| line[0]}
+
+pokey_things_by_first_letter.each do |letter, list|
+  puts "  #{letter}: #{list.map(&:chomp).join(',')}"
 end
 
 puts "\n--------\n\n"
 
-requests = LogData.new('./data/heroku.log')
-
 puts "chunking heroku requests by status code: "
-by_status = requests.chunk(&:status)
+by_status = @requests.chunk(&:status)
 
 by_status.each do |status, records|
   puts "  #{status}: #{records.map(&:id).join(',')}"
