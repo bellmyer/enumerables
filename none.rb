24fee293b556d5d82d20697babe52e9adf4d49b4
Jaime Bellmyer <online@bellmyer.com>
2014-09-07 15:30:06 -0500

    add all examples for none/one

--- none.rb
+++ none.rb
@@ -1,20 +1,41 @@
 #!/usr/bin/env ruby
 
-require './lib/pet_inventory'
-require './lib/pet'
+require './lib/initialize'
 
-inventory = PetInventory.new
-inventory.display
+print "no even numbers? "
+p @numbers.none?(&:even?)
+
+print "no number 15? "
+p @numbers.none?{|number| number == 5}
+
+divider
 
 print "no pets sold out? "
-puts inventory.none?(&:sold_out?)
+puts @inventory.none?(&:sold_out?)
 
 print "no pets in stock? "
-puts inventory.none?(&:in_stock?)
+puts @inventory.none?(&:in_stock?)
 
 print "no pets with three legs? "
-puts inventory.none?{|pet| pet.legs == 3}
+puts @inventory.none?{|pet| pet.legs == 3}
 
 print "no pets with four legs? "
-puts inventory.none?{|pet| pet.legs == 4}
+puts @inventory.none?{|pet| pet.legs == 4}
 
+divider
+
+print "no pokey things with the word cactus? "
+@pokey_things.seek(0)
+p @pokey_things.none?{|thing| thing =~ /cactus/}
+
+print "no pokey things that start with the letter z? "
+@pokey_things.seek(0)
+p @pokey_things.none?{|thing| thing[0] == 'z'}
+
+divider
+
+print "no errors? "
+p @requests.none?(&:error?)
+
+print "no 404 errors? "
+p @requests.none?{|request| request.status == 404}
--- one.rb
+++ one.rb
@@ -1,13 +1,35 @@
 #!/usr/bin/env ruby
 
-require './lib/pet_inventory'
-require './lib/pet'
+require './lib/initialize'
 
-inventory = PetInventory.new
-inventory.display
+print "only one even number? "
+p @numbers.one?(&:even?)
+
+print "only one number 5? "
+p @numbers.one?{|number| number == 5}
+
+divider
 
 print "only one pet with eight legs? "
-puts inventory.one?{|pet| pet.legs == 8}
+puts @inventory.one?{|pet| pet.legs == 8}
 
 print "only one pet with four legs? "
-puts inventory.one?{|pet| pet.legs == 4}
+puts @inventory.one?{|pet| pet.legs == 4}
+
+divider
+
+print "is there only one thing with the word cactus? "
+@pokey_things.seek(0)
+p @pokey_things.one?{|thing| thing =~ /cactus/}
+
+print "is there exactly one thing that starts with the letter p? "
+@pokey_things.seek(0)
+p @pokey_things.one?{|thing| thing[0] == 'p'}
+
+divider
+
+print "exactly one error? "
+p @requests.one?(&:error?)
+
+print "exactly one GET request? "
+p @requests.one?{|request| request.method == 'GET'}
