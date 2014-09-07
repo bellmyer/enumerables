16563e5b8f155fa74b4eed2badf482144653fb39
Jaime Bellmyer <online@bellmyer.com>
2014-09-07 17:08:52 -0500

    add all examples to first

--- first.rb
+++ first.rb
@@ -1,13 +1,35 @@
 #!/usr/bin/env ruby
 
-require './lib/pet_inventory'
-require './lib/pet'
+require './lib/initialize'
 
-inventory = PetInventory.new
-inventory.display
+print "first number: "
+p @numbers.first
+
+print "first three numbers: "
+p @numbers.first(3)
+
+divider
 
 print "first pet on the list: "
-p inventory.first.name
+p @inventory.first.name
 
 print "first 3 pets on the list: "
-p inventory.first(3).map(&:name)
+p @inventory.first(3).map(&:name)
+
+divider
+
+print "first pokey thing: "
+@pokey_things.seek(0)
+p @pokey_things.first.chomp
+
+print "first three pokey things: "
+@pokey_things.seek(0)
+p @pokey_things.first(3).map(&:chomp)
+
+divider
+
+print "first heroku request: "
+p @requests.first.id
+
+print "first three requests: "
+p @requests.first(3).map(&:id)
