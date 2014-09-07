6dc9b549cedcf8abe0e549bb09bc4b401f6dc766
Jaime Bellmyer <online@bellmyer.com>
2014-09-07 16:59:44 -0500

    add all count examples

--- count.rb
+++ count.rb
@@ -1,13 +1,41 @@
 #!/usr/bin/env ruby
 
-require './lib/pet_inventory'
-require './lib/pet'
+require './lib/initialize'
 
-inventory = PetInventory.new
-inventory.display
+print "count numbers: "
+p @numbers.count
 
-print "types of pets: "
-puts inventory.count
+print "count even numbers: "
+p @numbers.count(&:even?)
 
-print "types of pets with four legs: "
-puts inventory.count{|pet| pet.legs == 4}
+print "count divisible by three: "
+p @numbers.count{|number| number % 3 == 0}
+
+divider
+
+print "count pets: "
+puts @inventory.count
+
+print "count in-stock pets: "
+puts @inventory.count(&:in_stock?)
+
+print "count pets with four legs: "
+puts @inventory.count{|pet| pet.legs == 4}
+
+divider
+
+print "count pokey things with at least 3 letters: "
+@pokey_things.seek(0)
+p @pokey_things.map(&:chomp).count{|word| word.size >= 3}
+
+print "count pokey things with the letter 'e': "
+@pokey_things.seek(0)
+p @pokey_things.count{|line| line.include?('e')}
+
+divider
+
+print "count heroku requests made via the GET method: "
+p @requests.count(&:get?)
+
+print "count heroku requests with errors: "
+p @requests.count{|request| request.error?}
--- detect.rb
+++ detect.rb
@@ -1,14 +1,36 @@
 #!/usr/bin/env ruby
 
-require './lib/pet_inventory'
-require './lib/pet'
+require './lib/initialize'
 
-inventory = PetInventory.new
-inventory.display
+print "first even number: "
+p @numbers.detect(&:even?)
+
+print "first number divisible by three: "
+p @numbers.detect{|number| number % 3 == 0}
+
+divider
 
 print "first pet with four legs: "
-puts inventory.detect{|pet| pet.legs == 4}.name
+puts @inventory.detect{|pet| pet.legs == 4}.name
 
 no_pet = Pet.new('none', 0, 0)
 print "first pet with 3 legs: "
-puts inventory.detect(lambda{no_pet}){|pet| pet.legs == 3}.name
+puts @inventory.detect(lambda{no_pet}){|pet| pet.legs == 3}.name
+
+divider
+
+print "first pokey thing with at least 3 letters: "
+@pokey_things.seek(0)
+p @pokey_things.map(&:chomp).detect{|word| word.size >= 3}
+
+print "first pokey thing with the letter 'e': "
+@pokey_things.seek(0)
+p @pokey_things.detect{|line| line.include?('e')}
+
+divider
+
+print "first heroku request made via the POST method: "
+p @requests.detect(&:post?).id
+
+print "first heroku request with error: "
+p @requests.detect{|request| request.error?}.id
