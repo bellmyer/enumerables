153fc90e0aaf6a3b649ed96adba3712b29b68338
Jaime Bellmyer <online@bellmyer.com>
2014-09-07 17:01:37 -0500

    add all examples for find_index

--- find_index.rb
+++ find_index.rb
@@ -1,16 +1,38 @@
 #!/usr/bin/env ruby
 
-require './lib/pet_inventory'
-require './lib/pet'
+require './lib/initialize'
 
-inventory = PetInventory.new
-inventory.display
+print "index of first even number: "
+p @numbers.find_index(&:even?)
+
+print "index of first number divisible by three: "
+p @numbers.find_index{|number| number % 3 == 0}
+
+divider
 
 print "index for scorpions: "
-puts inventory.find_index{|pet| pet.name == 'scorpion'}
+puts @inventory.find_index{|pet| pet.name == 'scorpion'}
 
 print "index for first four-legged pet: "
-puts inventory.find_index{|pet| pet.legs == 4}
+puts @inventory.find_index{|pet| pet.legs == 4}
 
 print "index for first pet with over 100 in stock: "
-puts inventory.find_index{|pet| pet.quantity > 100}
+puts @inventory.find_index{|pet| pet.quantity > 100}
+
+divider
+
+print "index of first pokey thing with at least 3 letters: "
+@pokey_things.seek(0)
+p @pokey_things.map(&:chomp).find_index{|word| word.size >= 3}
+
+print "index of first pokey thing with the letter 'e': "
+@pokey_things.seek(0)
+p @pokey_things.find_index{|line| line.include?('e')}
+
+divider
+
+print "index of first heroku request made via the POST method: "
+p @requests.find_index(&:post?)
+
+print "index of first heroku request with error: "
+p @requests.find_index{|request| request.error?}
