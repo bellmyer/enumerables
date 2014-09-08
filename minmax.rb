0620fef3fc339c1ca831a5addbec776280636178
Jaime Bellmyer <online@bellmyer.com>
2014-09-07 20:28:10 -0500

    add all examples to minmax/minmax_by

--- minmax.rb
+++ minmax.rb
@@ -1,10 +1,35 @@
 #!/usr/bin/env ruby
 
-require './lib/pet_inventory'
-require './lib/pet'
+require './lib/initialize'
 
-inventory = PetInventory.new
-inventory.display
+print "lowest/highest numbers: "
+p @numbers.minmax
 
-print "pets with minimum/maximum quantities: "
-p inventory.minmax.map(&:name)
+print "lowest/highest numbers when negative: "
+p @numbers.minmax{|a,b| 0 - a <=> 0 - b}
+
+divider
+
+print "pets with minimum/maximum quantity: "
+puts @inventory.minmax.map(&:name).join(', ')
+
+print "pets with fewest/most legs: "
+puts @inventory.minmax{|a,b| a.legs <=> b.legs}.map(&:name).join(', ')
+
+divider
+
+print "first/last pokey things, alphabetically: "
+@pokey_things.seek(0)
+puts @pokey_things.minmax.map(&:chomp).join(', ')
+
+print "smallest/largest pokey things by letter count: "
+@pokey_things.seek(0)
+puts @pokey_things.minmax{|a,b| a.length <=> b.length}.map(&:chomp).join(', ')
+
+divider
+
+print "heroku requests with lowest/highest response times: "
+p @requests.minmax.map(&:id)
+
+print "heroku requests with lowest/highest service times: "
+p @requests.minmax{|a,b| a.service <=> b.service}.map(&:id)
--- minmax_by.rb
+++ minmax_by.rb
@@ -1,10 +1,35 @@
 #!/usr/bin/env ruby
 
-require './lib/pet_inventory'
-require './lib/pet'
+require './lib/initialize'
 
-inventory = PetInventory.new
-inventory.display
+print "lowest/highest numbers: "
+p @numbers.minmax_by{|number| number}
 
-print "pets with minimum/maximum number of legs: "
-p inventory.minmax_by(&:legs).map(&:name)
+print "lowest/highest numbers when negative: "
+p @numbers.minmax_by{|number| 0 - number}
+
+divider
+
+print "pets with minimum/maximum quantity: "
+puts @inventory.minmax_by(&:quantity).map(&:name).join(', ')
+
+print "pets with fewest/most legs: "
+puts @inventory.minmax_by(&:legs).map(&:name).join(', ')
+
+divider
+
+print "first/last pokey things, alphabetically: "
+@pokey_things.seek(0)
+puts @pokey_things.minmax_by{|thing| thing}.map(&:chomp).join(', ')
+
+print "smallest/largest pokey things by letter count: "
+@pokey_things.seek(0)
+puts @pokey_things.minmax_by(&:length).map(&:chomp).join(', ')
+
+divider
+
+print "heroku requests with lowest/highest response times: "
+p @requests.minmax_by(&:response_time).map(&:id)
+
+print "heroku requests with lowest/highest service times: "
+p @requests.minmax_by{|request| request.service}.map(&:id)
