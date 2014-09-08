455e1ba7a7a9ebd2cdd03e88b41661c3abd2947e
Jaime Bellmyer <online@bellmyer.com>
2014-09-07 21:12:01 -0500

    add all drop/drop/while examples

--- drop.rb
+++ drop.rb
@@ -1,13 +1,35 @@
 #!/usr/bin/env ruby
 
-require './lib/pet_inventory'
-require './lib/pet'
+require './lib/initialize'
 
-inventory = PetInventory.new
-inventory.display
+print "skipping the first two numbers: "
+p @numbers.drop(2)
+
+print "skipping no numbers: "
+p @numbers.drop(0)
+
+divider
 
 print "skipping the first two pets: "
-p inventory.drop(2).map(&:name)
+p @inventory.drop(2).map(&:name)
 
-print "skipping nothing: "
-p inventory.drop(0).map(&:name)
+print "skipping no pets: "
+p @inventory.drop(0).map(&:name)
+
+divider
+
+print "skipping first pokey thing: "
+@pokey_things.seek(0)
+p @pokey_things.drop(1).map(&:chomp)
+
+print "skipping no pokey things: "
+@pokey_things.seek(0)
+p @pokey_things.drop(0).map(&:chomp)
+
+divider
+
+print "skipping first 3 heroku requests: "
+p @requests.drop(3).map(&:id)
+
+print "skipping no heroku requests: "
+p @requests.drop(0).map(&:id)
--- drop_while.rb
+++ drop_while.rb
@@ -1,10 +1,45 @@
 #!/usr/bin/env ruby
 
-require './lib/pet_inventory'
-require './lib/pet'
+require './lib/initialize'
 
-inventory = PetInventory.new
-inventory.display
+def prime? number
+  return true if number < 4
+  
+  (2...number).each do |factor|
+    return false if number % factor == 0
+  end
+  
+  true
+end
 
-puts "only pets that line up AFTER a big inventory: "
-p inventory.drop_while{|pet| pet.quantity < 500}.map(&:name)
+print "skipping until we find an even number: "
+p @numbers.drop_while(&:odd?)
+
+print "skipping until we find a non-prime number: "
+p @numbers.drop_while{|number| prime?(number)}
+
+divider
+
+print "skipping until we find a pet with low inventory: "
+p @inventory.drop_while{|pet| pet.quantity >= 50}.map(&:name)
+
+print "skipping until we find the first legless pet: "
+p @inventory.drop_while{|pet| pet.legs > 0}.map(&:name)
+
+divider
+
+print "skipping until we see a pokey thing with the letter 'f': "
+@pokey_things.seek(0)
+p @pokey_things.drop_while{|thing| !thing.include?('f')}.map(&:chomp)
+
+print "skipping until a pokey thing has more than 7 letters: "
+@pokey_things.seek(0)
+p @pokey_things.drop_while{|thing| thing.chomp.length < 7}.map(&:chomp)
+
+divider
+
+print "skipping until a request uses the POST method: "
+p @requests.drop_while{|request| request.method != 'POST'}.map(&:id)
+
+print "skipping until a request is successful: "
+p @requests.drop_while{|request| request.error?}.map(&:id)
