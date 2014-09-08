878d7e6f7a2756f03ab01caf4be383d3d2190d66
Jaime Bellmyer <online@bellmyer.com>
2014-09-07 21:22:07 -0500

    add all take/take_while examples

--- take.rb
+++ take.rb
@@ -1,10 +1,35 @@
 #!/usr/bin/env ruby
 
-require './lib/pet_inventory'
-require './lib/pet'
+require './lib/initialize'
 
-inventory = PetInventory.new
-inventory.display
+print "taking only the first two numbers: "
+p @numbers.take(2)
 
-print "the first three pets in the list: "
-p inventory.take(3).map(&:name)
+print "taking numbers: "
+p @numbers.take(0)
+
+divider
+
+print "taking the first two pets: "
+p @inventory.take(2).map(&:name)
+
+print "taking no pets: "
+p @inventory.take(0).map(&:name)
+
+divider
+
+print "taking first pokey thing: "
+@pokey_things.seek(0)
+p @pokey_things.take(1).map(&:chomp)
+
+print "taking no pokey things: "
+@pokey_things.seek(0)
+p @pokey_things.take(0).map(&:chomp)
+
+divider
+
+print "taking first 3 heroku requests: "
+p @requests.take(3).map(&:id)
+
+print "taking no heroku requests: "
+p @requests.take(0).map(&:id)
--- take_while.rb
+++ take_while.rb
@@ -1,14 +1,45 @@
 #!/usr/bin/env ruby
 
-require './lib/pet_inventory'
-require './lib/pet'
+require './lib/initialize'
 
-inventory = PetInventory.new
-inventory.display
+def prime? number
+  return true if number < 4
+  
+  (2...number).each do |factor|
+    return false if number % factor == 0
+  end
+  
+  true
+end
 
-print "pets that are listed before fish: "
-p inventory.take_while{|pet| pet.name != 'fish'}.map(&:name)
+print "taking until we find an even number: "
+p @numbers.take_while(&:odd?)
 
+print "taking until we find a non-prime number: "
+p @numbers.take_while{|number| prime?(number)}
 
-print "sort by quantity, list everything with less than 100 in stock: "
-p inventory.sort_by(&:quantity).take_while{|pet| pet.quantity < 100 }.map(&:name)
+divider
+
+print "taking until we find a pet with low inventory: "
+p @inventory.take_while{|pet| pet.quantity >= 50}.map(&:name)
+
+print "taking until we find the first legless pet: "
+p @inventory.take_while{|pet| pet.legs > 0}.map(&:name)
+
+divider
+
+print "taking until we see a pokey thing with the letter 'f': "
+@pokey_things.seek(0)
+p @pokey_things.take_while{|thing| !thing.include?('f')}.map(&:chomp)
+
+print "taking until a pokey thing has more than 7 letters: "
+@pokey_things.seek(0)
+p @pokey_things.take_while{|thing| thing.chomp.length < 7}.map(&:chomp)
+
+divider
+
+print "taking until a request uses the POST method: "
+p @requests.take_while{|request| request.method != 'POST'}.map(&:id)
+
+print "taking until a request is successful: "
+p @requests.take_while{|request| request.error?}.map(&:id)
