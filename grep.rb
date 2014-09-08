fc265a9f0433171f443fa0814bf5fd62b7f3e335
Jaime Bellmyer <online@bellmyer.com>
2014-09-08 00:23:15 -0500

    add all examples for grep

--- grep.rb
+++ grep.rb
@@ -1,13 +1,35 @@
 #!/usr/bin/env ruby
 
-require './lib/pet_inventory'
-require './lib/pet'
+require './lib/initialize'
 
-inventory = PetInventory.new
-inventory.display
+print "numbers matching 5-20: "
+p @numbers.grep(5..20)
 
-print "pets with the letter 'o' in the name: "
-p inventory.map(&:name).grep(/o/)
+print "numbers matching 5-20, multiplied by 2: "
+p @numbers.grep(5..20){|x| x * 2}
 
-print "first letter of each pet with the letter 'i' in the name: "
-p inventory.map(&:name).grep(/i/){|name| name[0]}
+divider
+
+print "all pet names with the letter o: "
+p @inventory.map(&:name).grep(/o/)
+
+print "all pet names with the letter o, capitalized: "
+p @inventory.map(&:name).grep(/o/){|name| name.capitalize}
+
+divider
+
+print "pokey things involving cactus: "
+@pokey_things.seek(0)
+p @pokey_things.grep(/cactus/).map(&:chomp)
+
+print "pokey things involving cactus, capitalized: "
+@pokey_things.seek(0)
+p @pokey_things.grep(/cactus/){|thing| thing.chomp.capitalize}
+
+divider
+
+print "javascript requests in heroku logs: "
+p @requests.map(&:path).grep(/javascript/)
+
+print "javascript requests in heroku logs, shortened: "
+p @requests.map(&:path).grep(/javascript/){|path| path.gsub(%r!/javascript/!, '')}
